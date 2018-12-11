098a3ca48e2b39f54f5fd025d8cb97f20cf13729
Julius S <jluuiss@gmail.com>
2018-12-11 16:26:13 +0800

    Org-init-ize config

--- init.el
+++ init.el
@@ -1,41 +1,12 @@
-(setq inhibit-startup-message t)
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
-
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;;Bootstrap `use-package'
+;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+	(package-refresh-contents)
+	(package-install 'use-package))
 
-(use-package try
-  :ensure t)
-
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
-
-(use-package avy
-  :ensure t
-  :config
-  (avy-setup-default))
-
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-
-;; Aesthetics
-(use-package solarized-theme
-  :ensure t
-  :config
-  (load-theme 'solarized-dark t))
-
-(set-default-font "Inconsolata-g for Powerline")
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
