b6f1e9f0352e19d91b77ba416daac18ac0cc0242
Julius S <jluuiss@gmail.com>
2020-04-06 19:45:17 +0800

    Fix Python version stuff

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-fill-break-at-separators nil)
+ '(TeX-auto-save t)
+ '(TeX-parse-self t)
+ '(TeX-source-correlate-start-server nil)
+ '(TeX-syntactic-comment t t)
+ '(TeX-view-program-list (quote (("zathura" "zathura --page=%(outpage) %o"))))
+ '(TeX-view-program-selection (quote ((output-pdf "zathura"))))
+ '(package-selected-packages
+   (quote
+    (dracula-theme yasnippet-snippets which-key wgrep web-mode vlf utop use-package tuareg try treemacs tide solarized-theme request reason-mode rbenv proof-general projectile-rails ox-reveal ox-hugo ox-gfm nov multi-term magit lsp-ui lsp-python-ms lsp-haskell lcr htmlize go-mode gnu-elpa-keyring-update find-file-in-project exec-path-from-shell elpy dumb-jump doom-modeline csharp-mode counsel company-lsp cdlatex auto-complete auctex alchemist))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
