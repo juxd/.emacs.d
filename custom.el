33ae55db6e13fe9d599a4c53653c12919670e265
Julius S <jluuiss@gmail.com>
2020-04-12 17:01:34 +0800

    Fix custom.el

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit lsp-elixir elixir-lsp elixir-ls yasnippet which-key use-package try tide reason-mode ox-reveal lsp-ui lsp-python-ms lsp-haskell htmlize dracula-theme doom-modeline counsel company-lsp avy))))
+    (magit lsp-elixir yasnippet-snippets which-key wgrep web-mode vlf utop use-package tuareg try treemacs tide solarized-theme request reason-mode rbenv proof-general projectile-rails ox-reveal ox-hugo ox-gfm nov multi-term magit lsp-ui lsp-python-ms lsp-haskell lcr htmlize go-mode gnu-elpa-keyring-update find-file-in-project exec-path-from-shell elpy dumb-jump dracula-theme doom-modeline csharp-mode counsel company-lsp cdlatex auto-complete auctex alchemist))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
