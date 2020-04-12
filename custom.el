623bf09c8120248882507bd00c2fba19e403e9e7
Julius S <jluuiss@gmail.com>
2020-04-13 00:31:16 +0800

    Add use packages, add elixir lsp

--- custom.el
+++ custom.el
@@ -3,16 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(LaTeX-fill-break-at-separators nil)
- '(TeX-auto-save t)
- '(TeX-parse-self t)
- '(TeX-source-correlate-start-server nil)
- '(TeX-syntactic-comment t t)
- '(TeX-view-program-list (quote (("zathura" "zathura --page=%(outpage) %o"))))
- '(TeX-view-program-selection (quote ((output-pdf "zathura"))))
  '(package-selected-packages
    (quote
-    (dracula-theme yasnippet-snippets which-key wgrep web-mode vlf utop use-package tuareg try treemacs tide solarized-theme request reason-mode rbenv proof-general projectile-rails ox-reveal ox-hugo ox-gfm nov multi-term magit lsp-ui lsp-python-ms lsp-haskell lcr htmlize go-mode gnu-elpa-keyring-update find-file-in-project exec-path-from-shell elpy dumb-jump doom-modeline csharp-mode counsel company-lsp cdlatex auto-complete auctex alchemist))))
+    (magit lsp-elixir elixir-lsp elixir-ls yasnippet which-key use-package try tide reason-mode ox-reveal lsp-ui lsp-python-ms lsp-haskell htmlize dracula-theme doom-modeline counsel company-lsp avy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
